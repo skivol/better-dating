69093f023158324b23c202043f94b3bff2309fc2
Ivan Skachkov <ivan.skachkov.1992@gmail.com>
2019-06-30 22:47:10 +0200

    Initial commit
    * project (gradle/kotlin/spring boot/jar/java 12)
    * generated using https://start.spring.io

---
+++ better-dating-backend/src/main/kotlin/ua/betterdating/backend/BackendApplication.kt
@@ -0,0 +1,11 @@
+package ua.betterdating.backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<BackendApplication>(*args)
+}
